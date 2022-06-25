27ed73cba88cc7de3ecb1c2cfd3b8a408acb8a7b
NPScript <nathan@reinerweb.ch>
2022-06-25 12:32:14 +0200

    apply new directory structure

--- src/uri.php
+++ src/uri.php
@@ -1,36 +1,38 @@
 <?php
 
-$config = parse_ini_file('config.ini');
-
 $md = "";
 
 function snipped($name) {
-	global $md, $config;
-	include $config['html'] . '/' . $name . '.php';
+	global $md;
+	include '_snippets/' . $name . '.php';
+}
+
+function layout($name) {
+	global $md;
+	include '_layouts/' . $name . '.php';
 }
 
 function load_uri($path) {
-	global $md, $config;
+	global $md;
 	$md = new MarkdownFile($path);
 
 	$header = $md->header();
 	$layout = $header->item('layout');
 	$layout_path = $md->header()->item('layout')->to_string();
 
-	snipped($layout_path);
+	layout($layout_path);
 }
 
 function handle_uri() {
-	global $config;
 	$request = $_SERVER['REQUEST_URI'];
-	$uri = $config['markdown'] . '/' . $request;
+	$uri = 'content/' . $request;
 
 	if (is_dir($uri)) {
 		load_uri($uri . '/index.md');
 	} else if (file_exists($uri . '.md')) {
 		load_uri($uri . '.md');
 	} else {
-		load_uri($config['markdown'] . '/404.md');
+		load_uri('content/404.md');
 	}
 }
 
